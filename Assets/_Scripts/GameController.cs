86bafbe53a7e281fe61d2b154d940d56ff790f77
Chadwick Lapis <c_lapis4@hotmail.com>
2021-02-13 20:55:34 -0500

    Menus Added
    main menu and pause menu added, partly functional

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //pauses game
+        if (Input.GetButton("Cancel"))
+        {
+            pauseMenu.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+}
