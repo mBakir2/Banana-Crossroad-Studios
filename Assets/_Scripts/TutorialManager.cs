afc2ea74dfe74704b2cb6d9d3e8cd6ae01f69932
Anmoldeep Singh Gill <gillasg788@gmail.com>
2021-04-15 21:19:39 -0400

    Added Tutorial Game Logic

--- Assets/_Scripts/TutorialManager.cs
+++ Assets/_Scripts/TutorialManager.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class TutorialManager : MonoBehaviour
 {
     [Header("Buttons")]
-    public Joystick leftJoystick;
-    public Joystick rightJoystick;
+    public GameObject leftJoystickMove;
+    public GameObject rightJoystickMove;
     public GameObject shootButton;
     public GameObject jumpButton;
     public GameObject inventoryButton;
@@ -25,21 +25,98 @@ public class TutorialManager : MonoBehaviour
     public GameObject pauseButtonPopup;
     public GameObject sideInventoryPopup;
 
+    private Quaternion initialJoyStickRotation = new Quaternion(0, 0, 0, 0);
+    private Vector3 initialplayerPosition = new Vector3(0, 0, 0);
+
+    private bool rightJoystickPopupShown = false;
+    private bool leftJoystickPopupShown = true;
+    private bool jumpButtonPopupShown = true;
+    private bool achivementsButtonPopupShown = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        if (GameData.loadTutorial)
-        {
-            Debug.Log("Loading the tutorial");
-        }
+        initialJoyStickRotation = rightJoystickMove.transform.localRotation;
+        initialplayerPosition = leftJoystickMove.transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (GameData.loadTutorial)
-       {
-            
-       }
+        if (!rightJoystickPopupShown)
+        {
+            ActivateRightJoystickPopup();
+        }
+
+        if (!leftJoystickPopupShown)
+        {
+            ActivateLeftJoystickPopup();
+        }
+
+        if (!jumpButtonPopupShown)
+        {
+            ActivateJumpPopup();
+        }
+
+        if (!achivementsButtonPopupShown)
+        {
+            ActivateAchievementPopup();
+        }
     }
+
+    private void ActivateRightJoystickPopup()
+    {
+        rightJoystickPopups.gameObject.SetActive(true);
+        if (initialJoyStickRotation != rightJoystickMove.transform.localRotation)
+        {
+            rightJoystickPopups.gameObject.SetActive(false);
+            initialJoyStickRotation = new Quaternion(0, 0, 0, 0);
+            rightJoystickPopupShown = true;
+            leftJoystickPopupShown = false;
+        }
+    }
+
+    private void ActivateLeftJoystickPopup()
+    {
+        leftJoystickPopups.gameObject.SetActive(true);
+        if (initialplayerPosition.x != leftJoystickMove.transform.localPosition.x || initialplayerPosition.z != leftJoystickMove.transform.localPosition.z)
+        {
+            leftJoystickPopups.gameObject.SetActive(false);
+            initialplayerPosition = new Vector3(0, 0, 0);
+            leftJoystickPopupShown = true;
+            jumpButtonPopupShown = false;
+        }
+    }
+
+    private void ActivateJumpPopup()
+    {
+        bool jumpNotClickedOnce = true;
+        jumpPopup.gameObject.SetActive(true);
+        jumpButton.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            if (jumpNotClickedOnce)
+            {
+                jumpPopup.gameObject.SetActive(false);
+                jumpNotClickedOnce = false;
+                jumpButtonPopupShown = true;
+                achivementsButtonPopupShown = false;
+            }
+        });
+    }
+
+    private void ActivateAchievementPopup()
+    {
+        bool achievementNotClickedOnce = true;
+        achivementsPopup.gameObject.SetActive(true);
+        achivementsButton.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            if (achievementNotClickedOnce)
+            {
+                achivementsPopup.gameObject.SetActive(false);
+                achievementNotClickedOnce = false;
+                achivementsButtonPopupShown = true;
+            }
+        });
+    }
+
 }
